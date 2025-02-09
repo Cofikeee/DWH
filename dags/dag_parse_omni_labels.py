d90aef5169ad6cd2d0addb82c73740b8d086262d
Petr Bushmanov <bushmanov.pa@gmail.com>
2025-02-09 19:17:19 +0300

    Рефакторинг с использованием классов, добавлены комментарии и логи

--- dags/dag_parse_omni_labels.py
+++ dags/dag_parse_omni_labels.py
@@ -1,80 +1,110 @@
+# Airflow
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-
+# Прочие библиотеки
 import aiohttp
 import asyncio
 import asyncpg
-
+# Конфиг
 from config import DB_CONFIG, OMNI_URL, OMNI_LOGIN, OMNI_PASSWORD, DAG_CONFIG
-from functions import functions_general as fg
+# Классы
+from classes.omni_label import OmniLabel
+# Запросы к БД
 from queries import queries_log as ql, queries_insert as qi
-
-
-def labels_data_extractor(record):
-    # Извлечение и предобработка данных
-    return (
-        record.get('label_id'),
-        record.get('label_title')
-    )
+# Функции
+from functions import functions_general as fg, function_logging as fl
 
 
 async def fetch_and_process_labels():
     """
-    Основная функция для получения данных о метках и их вставки в базу данных.
+    Асинхронная функция для извлечения и обработки данных о метках из API Omni.
+    Логика работы:
+    1. Извлекает данные страницами из API Omni.
+    2. Обрабатывает каждую запись с использованием класса OmniLabel.
+    3. Вставляет обработанные данные в базу данных.
+    4. Логирует процесс извлечения и обработки данных.
     """
+    page = 1
+    batch_size = 5  # Размер пакета страниц для параллельной обработки
+
+    # Инициализация логгера
+    logger = fl.setup_logger('dag_parse_omni_labels')
+    logger.info('---------------------------------------')
+    logger.info('Начало работы DAG dag_parse_omni_labels')
+
+    # Создаем асинхронные сессии для HTTP-запросов и подключения к БД
     async with aiohttp.ClientSession(auth=aiohttp.BasicAuth(OMNI_LOGIN, OMNI_PASSWORD)) as session, \
             asyncpg.create_pool(**DB_CONFIG, min_size=5, max_size=20) as pool:
+        # Получаем соединение с БД
         async with pool.acquire() as conn:
+            # Получаем общее количество меток
             total_count = await fg.get_snapshot(session, 'labels')
-            try:
-                tasks = []
-                page = 1
 
-                while True:
-                    print('-', page)
+            while True:
+                # Очищаем список для хранения меток текущего пакета
+                batch_labels = []
+
+                # Переходим к параллельной обработке
+                for i in range(batch_size):
+                    # URL для запроса страницы
                     url = f'{OMNI_URL}/labels.json?page={page}&limit=100'
-                    task = fg.fetch_response(session, url)
-                    tasks.append(task)  # Добавляем задачу для парсинга.
+                    data = await fg.fetch_response(session, url)
 
-                    if len(tasks) >= 5:  # Обработка 5 страниц одновременно
-                        responses = await asyncio.gather(*tasks)
-                        tasks = []  # Очистка задач
-                        for response in responses:
-                            if response is None or len(response) <= 1:
-                                print('Все данные обработаны.')
-                                return
+                    # Проверяем полученные данные
+                    if not data or len(data) <= 1:
+                        logger.error('Получили неожиданный результат - пустую страницу.')
+                        raise Exception('Получили неожиданный результат - пустую страницу.')
 
-                            response_data = fg.fetch_data(response, labels_data_extractor, 'label')  # Извлечение данных
-                            await qi.insert_labels(conn, response_data)  # Вставка данных в базу.
+                    # Определяем общее количество записей и страниц для текущего периода
+                    if page == 1:
+                        period_total = int(data.get("total_count", 0))
+                        period_pages = (period_total + 99) // 100
 
-                    page += 1  # Переходим к следующей странице.
+                    # Очищаем список данных на текущей странице
+                    labels_data = []
+                    for item in data.values():
+                        if isinstance(item, dict) and "label" in item:
+                            label = OmniLabel(item["label"])
+                            processed_label = label.label_properties()
+                            if processed_label:
+                                labels_data.append(processed_label)
 
-            finally:
-                if tasks:  # Обрабатываем любые оставшиеся задачи.
-                    responses = await asyncio.gather(*tasks)
-                    for response in responses:
-                        if response is None or len(response) <= 1:
-                            print('Все данные обработаны.')
-                            return
+                    # Добавляем обработанные записи в пакет
+                    batch_labels.extend(labels_data)
 
-                await ql.log_etl_catalogues(conn, 'dim_omni_label', total_count)
-                await conn.close()  # Закрываем соединение с базой данных.
+                    # Переходим к следующей странице или завершаем обработку текущего периода, если страница последняя
+                    page += 1
+                    if page > period_pages:
+                        break
+
+                # Вставка данных в БД
+                if batch_labels:
+                    await qi.insert_labels(conn, batch_labels)
+
+                # Логируем завершение обработки текущего пакета
+                logger.info(f'Собраны данные за пакет страниц ({page-1}/{period_pages}).')
+
+                if page > period_pages:
+                    # Передаем в БД снэпшот количества меток для валидации в дальнейшем
+                    await ql.log_etl_catalogues(conn, 'dim_omni_label', total_count)
+                    logger.info(f'Собраны все данные по меткам.')
+                    return
 
 
 def run_async_func():
-    """Запускает асинхронную функцию для получения и вставки данных о метках."""
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(fetch_and_process_labels())
+    """
+    Запускает основную асинхронную функцию fetch_and_process_labels.
+    """
+    asyncio.run(fetch_and_process_labels())
 
 
-# Создаем DAG
+# Создание DAG для Airflow
 with DAG(
-        'dag_parse_omni_labels',
-        default_args=DAG_CONFIG,
-        schedule_interval=None,  # Не запускать автоматически
-        catchup=False,
+    'dag_parse_omni_labels',
+    default_args=DAG_CONFIG,  # Подгружаем настройки из конфига
+    catchup=False,            # Не выполнять пропущенные интервалы
+    schedule_interval=None,   # Не запускать автоматически
 ) as dag:
-
     fetch_labels_task = PythonOperator(
         task_id='parse_omni_labels',
         python_callable=run_async_func,
