6e833c635f45a4d322f13085952e2c9c171b0c0e
Petr Bushmanov <bushmanov.pa@gmail.com>
2025-02-09 19:30:58 +0300

    Рефакторинг с использованием классов, добавлены комментарии и логи

--- dags/dag_parse_omni_custom_fields.py
+++ dags/dag_parse_omni_custom_fields.py
@@ -1,77 +1,137 @@
+# Airflow
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-
+# Прочие библиотеки
 import aiohttp
 import asyncio
 import asyncpg
-import json
-
+# Конфиг
 from config import DB_CONFIG, OMNI_URL, OMNI_LOGIN, OMNI_PASSWORD, DAG_CONFIG
-from functions import functions_general as fg
+# Классы
+from classes.omni_custom_field import OmniCustomField
+# Запросы к БД
 from queries import queries_log as ql, queries_insert as qi
+# Функции
+from functions import functions_general as fg
+from functions.function_logging import setup_logger
 
 
-def field_data_extractor(record):
-    return (
-        record.get('field_id'),
-        record.get('title'),
-        record.get('field_type'),
-        record.get('field_level'),
-        record.get('active'),
-        json.dumps(record.get('field_data'))
-    )
+async def insert_into_all_dimensions(conn, logger):
+    """
+    Асинхронная функция для передачи данных в таблицы-словари, в которых используются пользовательские поля.
+    Логика работы:
+    1. Получает данные из таблицы lookup_omni_custom_field по field_id.
+    2. Создает индивидуальные записи в dim_ таблицах.
+    """
+    logger.info('Начало передачи данных в таблицы-словари.')
 
-
-async def insert_into_all_dimensions(conn):
-    print('Заполняю: dim_omni_category')
     await qi.insert_dimension(conn, 'dim_omni_category', '6998', 'category_id', 'category_name')
-    print('Заполняю: dim_omni_block')
+    logger('Переданы данные в dim_omni_category.')
+
     await qi.insert_dimension(conn, 'dim_omni_block', '4605', 'block_id', 'block_name')
-    print('Заполняю: dim_omni_topic')
+    logger('Переданы данные в dim_omni_block.')
+
     await qi.insert_dimension(conn, 'dim_omni_topic', '8497', 'topic_id', 'topic_name')
-    print('Заполняю: dim_omni_task')
+    logger('Переданы данные в dim_omni_topic.')
+
     await qi.insert_dimension(conn, 'dim_omni_task', '9129', 'task_id', 'task_name')
+    logger('Переданы данные в dim_omni_task.')
+
 
 
 async def fetch_and_process_custom_fields():
     """
-    Основная функция для получения данных о кастом филдах и их вставки в базу данных.
+    Асинхронная функция для извлечения и обработки данных о пользовательских полях из API Omni.
+    Логика работы:
+    1. Извлекает данные страницами из API Omni.
+    2. Обрабатывает каждую запись с использованием класса OmniCustomField.
+    3. Вставляет обработанные данные в базу данных.
+    4. Логирует процесс извлечения и обработки данных.
     """
+    page = 1
+    batch_size = 5  # Размер пакета страниц для параллельной обработки
+    # Инициализация логгера
+    logger = setup_logger('dag_parse_omni_custom_fields')
+    logger.info('---------------------------------------')
+    logger.info('Начало работы DAG dag_parse_omni_custom_fields')
 
+    # Создаем асинхронные сессии для HTTP-запросов и подключения к БД
     async with aiohttp.ClientSession(auth=aiohttp.BasicAuth(OMNI_LOGIN, OMNI_PASSWORD)) as session, \
             asyncpg.create_pool(**DB_CONFIG, min_size=5, max_size=20) as pool:
+        # Получаем соединение с БД
         async with pool.acquire() as conn:
+            # Получаем общее количество пользовательских полей
             total_count = await fg.get_snapshot(session, 'custom_fields')
-            try:
-                url = f'{OMNI_URL}/custom_fields.json?limit=100'  # Формируем ссылку для API запроса
-                response = await fg.fetch_response(session, url)   # Создание запроса для получения данных
-                response_data = fg.fetch_data(response, field_data_extractor, 'custom_field')  # Извлечение данных
-                await qi.insert_custom_fields(conn, response_data)  # Вставка данных в базу.
-                await insert_into_all_dimensions(conn)     # Вставка категорий, блоков, тем и заданий в базу.
-                print("Все данные обработаны.")
 
-            finally:
-                await ql.log_etl_catalogues(conn, 'lookup_omni_custom_field', total_count)
-                await conn.close()  # Закрываем соединение с базой данных.
+            while True:
+                # Очищаем список для хранения пользовательских полей текущего пакета
+                batch_custom_fields = []
+
+                # Переходим к параллельной обработке
+                for i in range(batch_size):
+                    # URL для запроса страницы
+                    url = f'{OMNI_URL}/custom_fields.json?page={page}&limit=100'
+                    data = await fg.fetch_response(session, url)
+
+                    # Проверяем полученные данные
+                    if not data or len(data) <= 1:
+                        logger.error('Получили неожиданный результат - пустую страницу.')
+                        raise Exception('Получили неожиданный результат - пустую страницу.')
+
+                    # Определяем общее количество записей и страниц для текущего периода
+                    if page == 1:
+                        period_total = int(data.get("total_count", 0))
+                        period_pages = (period_total + 99) // 100
+
+                    # Очищаем список данных на текущей странице
+                    custom_fields_data = []
+                    for item in data.values():
+                        if isinstance(item, dict) and "custom_field" in item:
+                            custom_field = OmniCustomField(item["custom_field"])
+                            processed_custom_field = custom_field.custom_field_properties()
+                            if processed_custom_field:
+                                custom_fields_data.append(processed_custom_field)
+
+                    # Добавляем обработанные записи в пакет
+                    batch_custom_fields.extend(custom_fields_data)
+
+                    # Переходим к следующей странице или завершаем обработку текущего периода, если страница последняя
+                    page += 1
+                    if page > period_pages:
+                        break
+
+                # Вставка данных в БД
+                if batch_custom_fields:
+                    await qi.insert_custom_fields(conn, batch_custom_fields)
+                    await insert_into_all_dimensions(conn, logger)
+
+                # Логируем завершение обработки текущего пакета
+                logger.info(f'Собраны данные за пакет страниц ({page-1}/{period_pages}).')
+
+                if page > period_pages:
+                    # Передаем в БД снэпшот количества пользовательских полей для валидации в дальнейшем
+                    await ql.log_etl_catalogues(conn, 'lookup_omni_custom_field', total_count)
+                    logger.info(f'Собраны все данные по пользовательским полям.')
+                    return
 
 
 def run_async_func():
-    """Запускает асинхронную функцию для получения и вставки данных о кастом филдах."""
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(fetch_and_process_custom_fields())
+    """
+    Запускает основную асинхронную функцию fetch_and_process_custom_fields.
+    """
+    asyncio.run(fetch_and_process_custom_fields())
 
 
-# Создаем DAG
+# Создание DAG для Airflow
 with DAG(
-        'dag_parse_omni_custom_fields',
-        default_args=DAG_CONFIG,
-        schedule_interval=None,  # Не запускать автоматически
-        catchup=False,
+    'dag_parse_omni_custom_fields',
+    default_args=DAG_CONFIG,  # Подгружаем настройки из конфига
+    catchup=False,            # Не выполнять пропущенные интервалы
+    schedule_interval=None,   # Не запускать автоматически
 ) as dag:
-
-    fetch_custom_field_task = PythonOperator(
+    fetch_custom_fields_task = PythonOperator(
         task_id='parse_omni_custom_fields',
         python_callable=run_async_func,
     )
 
-    fetch_custom_field_task
+    fetch_custom_fields_task
