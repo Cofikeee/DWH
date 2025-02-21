fab59c5760de491285b6575af2dcf79027b18366
Petr Bushmanov <bushmanov.pa@gmail.com>
2025-02-21 20:32:07 +0300

    Апдейт, рефакторинг config

--- dags/config.py
+++ dags/config.py
@@ -9,37 +9,57 @@ GLOBAL_PAUSE.set()  # Изначально установим в "работае
 
 load_dotenv()
 
-placeholder_date = datetime(2024, 1, 1)
-
-# omni
+# КРЕДЫ ПОЛЬЗОВАТЕЛЯ "OMNI"
 OMNI_USER = os.getenv("OMNI_USER")
 OMNI_LOGIN = os.getenv("OMNI_LOGIN")
 OMNI_PASSWORD = os.getenv("OMNI_PASSWORD")
 
-# database
-OMNI_DB_NAME = os.getenv("OMNI_DB_NAME")
-REAPER_DB_NAME = os.getenv("REAPER_DB_NAME")
+# МОИ КРЕДЫ
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 
-OMNI_DB_CONFIG = {
-    "user": DB_USER,          # имя пользователя PostgreSQL
-    "password": DB_PASSWORD,  # пароль PostgreSQL
-    "host": DB_HOST,          # адрес БД
-    "port": DB_PORT,          # порт БД
-    "database": OMNI_DB_NAME  # имя БД
+# REAPER
+REAPER_DB_NAME = os.getenv("REAPER_DB_NAME")
+DB_CONFIG = {
+    "user": DB_USER,
+    "password": DB_PASSWORD,
+    "host": DB_HOST,
+    "port": DB_PORT,
+    "database": REAPER_DB_NAME
 }
+REAPER_DB_DSN = f"user={DB_USER} password={DB_PASSWORD} host={DB_HOST} port={DB_PORT} dbname={REAPER_DB_NAME}"
 
+# OMNIDESK
+OMNI_DB_NAME = os.getenv("OMNI_DB_NAME")
+OMNI_DB_CONFIG = {
+    "user": DB_USER,
+    "password": DB_PASSWORD,
+    "host": DB_HOST,
+    "port": DB_PORT,
+    "database": OMNI_DB_NAME
+}
 OMNI_DB_DSN = f"user={DB_USER} password={DB_PASSWORD} host={DB_HOST} port={DB_PORT} dbname={OMNI_DB_NAME}"
 
-
+# КОНФИГИ DAG-ОВ
 DAG_CONFIG = {
     'owner': '@pbushmanov',
     'start_date': datetime(2025, 1, 1),  # Дата начала
 }
 
+FIRST_DATE = datetime(2020, 10, 1)
+
+# КОНФИГИ ОБХОДЧИКОВ
+COLORS = ['green', 'blue', 'black', 'pink', 'gold']
+COLORS_SEMAPHORES = {'green': 4,
+                     'blue': 20,
+                     'black': 12,
+                     'pink': 2,
+                     'gold': 2}
+
+# КОНФИГИ OMNIDESK
+OMNI_URL = "https://hr-link.omnidesk.ru/api"
 BLACKLIST_LIST = [
     "ILOVEHRLINK4EVER&FORMOFRESPONSEABOUTHRLINK",
     "Поступление на счёт в валюте счёта",
@@ -54,15 +74,19 @@ BLACKLIST_LIST = [
     "Support:.*",
     "Бухгалтерия"
 ]
-
 BLACKLIST = re.compile("|".join(BLACKLIST_LIST))
 
-# urls
-OMNI_URL = "https://hr-link.omnidesk.ru/api"
-
-# parser settings
+# КОНФИГ ДЛЯ КОНТРОЛЯ ЛИМИТОВ ПАРСЕРА OMNIDESK
 DELAY_BETWEEN_REQUESTS = 0.08
 WORKERS = 5
 OFFSET_VALUE = 0
 OFFSET_SKEW = 20
 QUEUE_SIZE = 10
+
+# КОНФИГ SSH-ТУННЕЛЯ ДЛЯ ПРОКСИ ДЖАМПА
+SSH_PROXY_HOST = os.getenv("SSH_PROXY_HOST")
+SSH_PROXY_PORT = os.getenv("SSH_PROXY_PORT")
+SSH_USER = os.getenv("SSH_USER")
+SSH_PROXY_USER = os.getenv("SSH_PROXY_USER")
+SSH_PRIVATE_KEY_PATH = os.getenv("SSH_PRIVATE_KEY_PATH")
+SSH_PASSWORD = os.getenv("SSH_PASSWORD")
--- dags/queries/queries_select.py
+++ dags/queries/queries_select.py
@@ -5,7 +5,7 @@ from config import OMNI_DB_DSN
 
 
 async def select_missing_case_ids(conn, offset_skew):
-    case_ids = await conn.fetch("""
+    query = """
         SELECT distinct case_id
         FROM v_message_logs
         WHERE parsed_total <> period_total
@@ -23,54 +23,59 @@ async def select_missing_case_ids(conn, offset_skew):
             AND l.case_id = f.case_id)
         ORDER BY case_id
         LIMIT $1;
-    """, offset_skew)
+    """
+    case_ids = await conn.fetch(query, offset_skew)
     case_ids = [row['case_id'] for row in case_ids]
     return case_ids
 
 
 async def select_missing_catalogues(conn):
-    catalogues_array = await conn.fetch("""
+    query = """
         SELECT table_name
         FROM (SELECT table_name, max(parsed_date)
               FROM ctl_etl_omni_catalogues
               WHERE table_name <> 'dim_omni_user' and count_total > parsed_total
               GROUP BY table_name) as t
         ORDER BY table_name;
-    """)
+    """
+    catalogues_array = await conn.fetch(query)
     catalogues_array = [row['from_time'] for row in catalogues_array]
     return catalogues_array
 
 
 async def select_missing_case_dates(conn):
-    from_time_array = await conn.fetch("""
+    query = """
         SELECT distinct from_time
         FROM v_case_logs
         WHERE period_total_whitelisted <> parsed_total
         ORDER BY from_time;
-    """)
+    """
+    from_time_array = await conn.fetch(query)
     from_time_array = [row['from_time'] for row in from_time_array]
     return from_time_array
 
 
 async def select_missing_user_dates(conn):
-    from_time_array = await conn.fetch("""
+    query = """
         SELECT distinct from_time
         FROM v_user_logs
         WHERE period_total <> parsed_total
         ORDER BY from_time;
-    """)
+    """
+    from_time_array = await conn.fetch(query)
     from_time_array = [row['from_time'] for row in from_time_array]
     return from_time_array
 
 
 async def select_case_ids(conn, from_time, to_time, offset_skew, offset_value):
-    case_ids = await conn.fetch("""
+    query = """
         SELECT case_id FROM fact_omni_case
         WHERE updated_date >= $1
           AND updated_date < $2
         ORDER BY updated_date, created_date
         LIMIT $3 OFFSET $4;
-    """, from_time, to_time, offset_skew, offset_value)
+    """
+    case_ids = await conn.fetch(query, from_time, to_time, offset_skew, offset_value)
     case_ids = [row['case_id'] for row in case_ids]
     return case_ids
 
