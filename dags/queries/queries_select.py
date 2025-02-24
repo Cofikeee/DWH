bc0e03badbac9505e5813e2b74724b1fc9cc45f6
Petr Bushmanov <bushmanov.pa@gmail.com>
2025-02-24 20:14:54 +0300

    Добавлена функция select_column_names для получения названий колонок целевой таблицы.

--- dags/queries/queries_select.py
+++ dags/queries/queries_select.py
@@ -19,7 +19,7 @@ async def select_missing_case_ids(conn, offset_skew):
             WHERE d.case_id = f.case_id)
         AND NOT EXISTS (
             SELECT 1
-            FROM ctl_etl_omni_messages l
+            FROM ctl_etl_omni_message l
             WHERE period_total  = 0
             AND l.case_id = f.case_id)
         ORDER BY case_id
@@ -34,7 +34,7 @@ async def select_missing_catalogues(conn):
     query = """
         SELECT table_name
         FROM (SELECT table_name, max(parsed_date)
-              FROM ctl_etl_omni_catalogues
+              FROM ctl_etl_omni_catalogue
               WHERE table_name <> 'dim_omni_user' and count_total > parsed_total
               GROUP BY table_name) as t
         ORDER BY table_name;
@@ -99,6 +99,18 @@ async def select_max_value(conn, data_table, value_column):
     """
     max_value = await conn.fetchval(query)
     return max_value
+
+
+async def select_column_names(conn, table_name):
+    query = """
+    SELECT column_name
+    FROM information_schema.columns
+    WHERE table_name = $1;
+    """
+    result = await conn.fetch(query, table_name)
+    # Извлекаем названия колонок из результата
+    column_names = [row['column_name'] for row in result]
+    return column_names
 
 
 def select_max_ts(data_table, date_column):
