c161b6bf53d59411d535897f5bda67fae1965783
Petr Bushmanov <bushmanov.pa@gmail.com>
2025-03-19 14:00:53 +0300

    Добавлены select_ten_max_parsed_date, select_tenants_sync. Обновлена select_missing_case_ids для работы с логами.

--- dags/queries/queries_select.py
+++ dags/queries/queries_select.py
@@ -1,24 +1,42 @@
-async def select_missing_case_ids(conn, offset_skew):
+from config import OFFSET_SKEW
+
+
+async def select_missing_case_ids(conn, table):
     #         SELECT distinct case_id
-    #         FROM dwh_omni.v_ctl_message_logs
+    #         FROM dwh_log.v_ctl_omni_message_log
     #         WHERE parsed_total <> period_total
     #         UNION
-    query = """
-        SELECT distinct case_id
-        FROM dwh_omni.fact_omni_case f
-        WHERE NOT EXISTS (
-            SELECT 1
-            FROM dwh_omni.dim_omni_message d
-            WHERE d.case_id = f.case_id)
-        AND NOT EXISTS (
-            SELECT 1
-            FROM dwh_omni.ctl_etl_omni_message l
-            WHERE period_total  = 0
-            AND l.case_id = f.case_id)
-        ORDER BY case_id
-        LIMIT $1;
-    """
-    case_ids = await conn.fetch(query, offset_skew)
+    if table == 'dim_omni_message':
+        query = """
+            SELECT distinct case_id
+            FROM dwh_omni.fact_omni_case f
+            WHERE NOT EXISTS (
+                SELECT 1
+                FROM dwh_omni.dim_omni_message d
+                WHERE d.case_id = f.case_id
+                )
+            AND NOT EXISTS (
+                SELECT 1
+                FROM dwh_log.ctl_omni_message l
+                WHERE period_total  = 0
+                AND l.case_id = f.case_id
+                )
+            ORDER BY case_id
+            LIMIT $1;
+        """
+    elif table == 'dim_omni_changelog':
+        query = """
+            SELECT DISTINCT case_id
+            FROM dwh_omni.fact_omni_case
+            WHERE updated_date > '2024-03-01'
+              AND case_id NOT IN (
+                  SELECT DISTINCT case_id
+                  FROM dwh_omni.dim_omni_changelog
+                      )
+            ORDER BY case_id
+            LIMIT $1;
+        """
+    case_ids = await conn.fetch(query, OFFSET_SKEW)  # NOQA
     case_ids = [row['case_id'] for row in case_ids]
     return case_ids
 
@@ -27,7 +45,7 @@ async def select_missing_catalogues(conn):
     query = """
         SELECT table_name
         FROM (SELECT table_name, max(parsed_date)
-              FROM dwh_omni.ctl_etl_omni_catalogue
+              FROM dwh_log.ctl_omni_catalogue
               WHERE table_name <> 'dim_omni_user' and count_total > parsed_total
               GROUP BY table_name) as t
         ORDER BY table_name;
@@ -40,7 +58,7 @@ async def select_missing_catalogues(conn):
 async def select_missing_case_dates(conn):
     query = """
         SELECT distinct from_time
-        FROM dwh_omni.v_ctl_case_logs
+        FROM dwh_log.v_ctl_omni_case_log
         WHERE period_total_whitelisted <> parsed_total
         ORDER BY from_time;
     """
@@ -52,7 +70,7 @@ async def select_missing_case_dates(conn):
 async def select_missing_user_dates(conn):
     query = """
         SELECT distinct from_time
-        FROM dwh_omni.v_ctl_user_logs
+        FROM dwh_log.v_ctl_user_log
         WHERE period_total <> parsed_total
         ORDER BY from_time;
     """
@@ -95,6 +113,16 @@ async def select_max_value(conn, schema_name, data_table, value_column):
     return max_value
 
 
+async def select_ten_max_parsed_date(conn, data_table):
+    query = f"""
+    SELECT MAX(parsed_date)
+    FROM dwh_log.ctl_ten_cralwer
+    WHERE table_name = '{data_table}';
+    """
+    max_parsed_date = await conn.fetchval(query)
+    return max_parsed_date
+
+
 async def select_column_names(conn, table_name):
     query = """
     SELECT column_name
@@ -104,3 +132,19 @@ async def select_column_names(conn, table_name):
     result = await conn.fetch(query, table_name)
     column_names = [row['column_name'] for row in result]
     return column_names
+
+
+def select_tenants_sync(conn):
+    query = """
+    SELECT instance_name as color, db_host, datname, tenant_id, tenant_name, tenant_host
+    FROM public.v_tenant_database_replica
+    WHERE db_schema = 'ekd_ca'
+    ORDER BY datname;
+    """
+    cursor = conn.cursor()
+    # Выполнение запроса
+    cursor.execute(query)
+    result = cursor.fetchall()
+    # Закрытие курсора
+    cursor.close()
+    return result
