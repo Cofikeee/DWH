77d437574ff19777ab3957e064929c82463bcf21
Petr Bushmanov <bushmanov.pa@gmail.com>
2025-03-19 13:59:43 +0300

    Добавлена функция log_etl_tenants

--- dags/queries/queries_log.py
+++ dags/queries/queries_log.py
@@ -1,7 +1,10 @@
+from functions import functions_general as fg
+
+
 async def log_etl_cases(conn, from_time, to_time, page, parsed, blacklisted, period_total):
     page_total = parsed + blacklisted
     query = """
-        INSERT INTO dwh_omni.ctl_etl_omni_case (from_time, to_time, page, parsed, blacklisted, page_total, period_total, parsed_date)
+        INSERT INTO dwh_log.ctl_omni_case (from_time, to_time, page, parsed, blacklisted, page_total, period_total, parsed_date)
         VALUES ($1, $2, $3, $4, $5, $6, $7, NOW())
         ON CONFLICT (from_time, page) DO UPDATE
         SET to_time = EXCLUDED.to_time,
@@ -16,7 +19,7 @@ async def log_etl_cases(conn, from_time, to_time, page, parsed, blacklisted, per
 
 async def log_etl_users(conn, from_time, to_time, page, parsed, period_total):
     query = """
-        INSERT INTO dwh_omni.ctl_etl_omni_user (from_time, to_time, page, parsed, period_total, parsed_date)
+        INSERT INTO dwh_log.ctl_omni_user (from_time, to_time, page, parsed, period_total, parsed_date)
         VALUES ($1, $2, $3, $4, $5, NOW())
         ON CONFLICT (from_time, page) DO UPDATE
         SET to_time = EXCLUDED.to_time,
@@ -29,7 +32,7 @@ async def log_etl_users(conn, from_time, to_time, page, parsed, period_total):
 
 async def log_etl_messages(conn, case_id, page, parsed, period_total):
     query = """
-        INSERT INTO dwh_omni.ctl_etl_omni_message (case_id, page, parsed, period_total, parsed_date)
+        INSERT INTO dwh_log.ctl_omni_message (case_id, page, parsed, period_total, parsed_date)
         VALUES ($1, $2, $3, $4, NOW())
         ON CONFLICT (case_id, page) DO UPDATE
         SET parsed = EXCLUDED.parsed,
@@ -48,9 +51,18 @@ async def log_etl_catalogues(conn, table_name, total_count=None):
         raise ValueError(f"Недопустимое имя таблицы: {table_name}")
 
     query = f"""
-        INSERT INTO dwh_omni.ctl_etl_omni_catalogue (table_name, count_total, parsed_total, parsed_date)
+        INSERT INTO dwh_log.ctl_omni_catalogue (table_name, count_total, parsed_total, parsed_date)
         VALUES($1, $2, (SELECT count(1) FROM dwh_omni.{table_name}), date_trunc('day', now()))
         ON CONFLICT (table_name, parsed_date) DO UPDATE
         SET parsed_total = EXCLUDED.parsed_total
     """
     await conn.execute(query, table_name, total_count)
+
+
+async def log_etl_tenants(conn, table_name):
+    query = f"""
+        INSERT INTO dwh_log.ctl_ten_cralwer(table_name, parsed_date) 
+        VALUES($1, $2)
+        ON CONFLICT (table_name, parsed_date) DO NOTHING;
+    """
+    await conn.execute(query, table_name, fg.get_today())
