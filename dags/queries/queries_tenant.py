0100fc090f41eccd360522579557402238521294
Petr Bushmanov <bushmanov.pa@gmail.com>
2025-04-07 21:20:20 +0300

    Упрощена collect_agg_s_session_d, добавлены collect_agg_e_doc_type_m и collect_agg_e_app_type_m

--- dags/queries/queries_tenant.py
+++ dags/queries/queries_tenant.py
@@ -1,19 +1,17 @@
-async def collect_agg_n_sms_d(conn, from_created_date, to_created_date):
-    await conn.execute("SET LOCAL work_mem = '384MB';")
+import asyncpg
 
+
+async def collect_agg_n_sms_d(conn, from_created_date, to_created_date):
     query = f"""
-        WITH constants     AS (SELECT CLOCK_TIMESTAMP() AS ts)
         SELECT person.user_id AS dim_user_id,
                last_name || ' ' || first_name || ' ' || COALESCE(patronymic, ' ') AS dim_person_fio,
-               DATE_TRUNC('day', sms_notification.created_date) AS dim_start_of_day,
+               DATE_TRUNC('day', sms_notification.created_date)::DATE AS dim_start_of_day,
                COUNT(1) AS agg_cnt_all_sms,
                COUNT(CASE WHEN text ~* 'Код для входа|создания нового пароля|Для принятия приглашения' THEN 1 END) AS agg_cnt_auto_sms,
                COUNT(CASE WHEN text ~* 'кадровый документ|кадровые документы|Документ на ознакомление|Документы, ожидающие подписания|Документов, ожидающих|для подписания документа' THEN 1 END) AS agg_cnt_doc_sms,
                COUNT(CASE WHEN text ~* 'заявлени' THEN 1 END) AS agg_cnt_app_sms,
                COUNT(CASE WHEN text ~* 'Для удаленной идентификации|был изменен канал уведомлений|смену канала уведомлений|пользователь отключ|сотрудник подтвердил|сотрудник отклонил|УЦ сообщил об ошибке|для выпуска|(завершите.*подписи)|канал получения кода|для подтверждения|код подтверждения|смену канала' THEN 1 END) AS agg_cnt_emp_sms,
-               COUNT(CASE WHEN text ~* 'доверенност' THEN 1 END) AS agg_cnt_mchd_sms
-            , EXTRACT(EPOCH FROM CLOCK_TIMESTAMP() - (SELECT MIN(ts) FROM constants)) AS ctl_ts_delta
-
+               COUNT(CASE WHEN text ~* 'доверенност' THEN 1 END) AS agg_cnt_mchd_sms     
         FROM ekd_notification.sms_notification
              JOIN ekd_id.person ON person.user_id = sms_notification.user_id
         WHERE sms_notification.notification_status IN ('QUEUED', 'URL_SHORTENED', 'SENT')
@@ -22,19 +20,22 @@ async def collect_agg_n_sms_d(conn, from_created_date, to_created_date):
         GROUP BY dim_start_of_day, dim_user_id, dim_person_fio
         ORDER BY 1;
     """
-    return await conn.fetch(query)
+    try:
+        return await conn.fetch(query)
+    except Exception as e:
+        print(f"Ошибка при выполнении SQL-запроса: {e}")
+        raise
+
 
 
 async def collect_agg_s_session_d(conn, from_created_date, to_created_date):
-    await conn.execute("SET LOCAL work_mem = '384MB';")
-
     query = f"""
-        WITH
-            autorization AS (
-                SELECT user_id                         AS dim_user_id
-                     , id
-                     , created_date
-                     , DATE_TRUNC('day', created_date) AS dim_start_of_day
+         WITH
+             autorization AS (
+                 SELECT user_id                         
+                      , id
+                      , created_date
+                      , DATE_TRUNC('day', created_date)::DATE AS start_of_day
                      , CASE
                            WHEN user_agent ~* 'HRlinkAppAndroid|HRlinkAppIOS' THEN 'Мобильное приложение'
                            WHEN user_agent ~* 'Android|iPhone' THEN 'Телефон'
@@ -43,7 +44,7 @@ async def collect_agg_s_session_d(conn, from_created_date, to_created_date):
                            WHEN user_agent ~* 'PostmanRuntime' THEN 'PostmanRuntime'
                            WHEN user_agent ~* '1C' THEN '1C'
                            ELSE 'Неизвестно'
-                    END                                AS dim_device
+                    END                                AS device
                      , CASE
                            WHEN user_agent ~* 'HRlinkAppAndroid' THEN 'Приложение на Android'
                            WHEN user_agent ~* 'HRlinkAppIOS' THEN 'Приложение на IOS'
@@ -53,7 +54,7 @@ async def collect_agg_s_session_d(conn, from_created_date, to_created_date):
                            WHEN user_agent ~* 'Android' THEN 'Android'
                            WHEN user_agent ~* 'iPhone|iPad' THEN 'IOS'
                            ELSE 'Неизвестно'
-                    END                                AS dim_os
+                    END                                AS os
                      , CASE
                            WHEN user_agent ~* 'Firefox' THEN 'Mozilla Firefox'
                            WHEN user_agent ~* 'SamsungBrowser' THEN 'Samsung Internet'
@@ -66,52 +67,38 @@ async def collect_agg_s_session_d(conn, from_created_date, to_created_date):
                            WHEN user_agent ~* 'HRlinkAppAndroid' THEN 'Приложение на Android'
                            WHEN user_agent ~* 'HRlinkAppIOS' THEN 'Приложение на IOS'
                            ELSE 'Неизвестно'
-                    END                                AS dim_browser
-                FROM ekd_session.cookie_session
-                WHERE created_date >= '{from_created_date}'
-                  AND created_date < '{to_created_date}'
-            ),
-            min_sess     AS (
-                SELECT dim_user_id
-                     , dim_start_of_day
-                     , dim_device
-                     , dim_os
-                     , dim_browser
-                     , FIRST_VALUE(id) OVER (PARTITION BY dim_user_id, dim_start_of_day, dim_device, dim_os, dim_browser 
-                                                 ORDER BY created_date) AS ctl_min_session_id
-                FROM autorization
-            )
-        
-        SELECT dim_user_id
-             , dim_device
-             , dim_os
-             , dim_browser
-             , dim_start_of_day
-             , COUNT(ctl_min_session_id)
-             , ctl_min_session_id
-        FROM min_sess
-        
-        GROUP BY dim_user_id,
-                 dim_start_of_day,
-                 dim_device,
-                 dim_os,
-                 dim_browser,
-                 ctl_min_session_id;
-    """
-    return await conn.fetch(query)
+                    END                                AS browser
+                 FROM ekd_session.cookie_session
+                 WHERE created_date >= '{from_created_date}'
+                   AND created_date < '{to_created_date}'
+             )
+         SELECT user_id
+              , device
+              , os
+              , browser
+              , start_of_day
+              , COUNT(id) agg_cnt_session
+         FROM autorization
+
+         GROUP BY user_id,
+                  device,
+                  os,
+                  browser,
+                  start_of_day;
+         """
+    try:
+        return await conn.fetch(query)
+    except asyncpg.exceptions.PostgresError as e:
+        print(f"Ошибка PostgreSQL при выполнении запроса: {e.__class__.__name__} - {e}")
+        raise
 
 
 async def collect_agg_c_signing_notification_sms_d(conn, from_created_date, to_created_date):
-    await conn.execute("SET LOCAL work_mem = '384MB';")
-
     query = f"""
-            WITH constants     AS (SELECT CLOCK_TIMESTAMP() AS ts)
             SELECT person.user_id AS dim_user_id,
                    last_name || ' ' || first_name || ' ' || COALESCE(patronymic, ' ') AS dim_person_fio,
-                   DATE_TRUNC('day', ca_document_signing_request.created_date) AS dim_start_of_day,
+                   DATE_TRUNC('day', ca_document_signing_request.created_date)::DATE AS dim_start_of_day,
                    COUNT(DISTINCT ca_document_signing_request.id) AS agg_cnt_all_signing
-                , EXTRACT(EPOCH FROM CLOCK_TIMESTAMP() - (SELECT MIN(ts) FROM constants)) AS ctl_ts_delta
-
             FROM ekd_ca.ca_document_signing_request
                  JOIN ekd_ca.ca_certificate ON LOWER(ca_certificate.fingerprint) = ca_document_signing_request.fingerprint
                  JOIN ekd_ca.nqes_issue_request ON ca_certificate.id = nqes_issue_request.ca_certificate_id
@@ -125,8 +112,6 @@ async def collect_agg_c_signing_notification_sms_d(conn, from_created_date, to_c
 
 
 async def collect_dim_user(conn, from_modified_date, to_modified_date):
-    await conn.execute("SET LOCAL work_mem = '384MB';")
-
     query = f"""
         WITH
             roles AS (
@@ -137,7 +122,7 @@ async def collect_dim_user(conn, from_modified_date, to_modified_date):
                                     THEN 'Администратор' END),
                         MAX(CASE
                                 WHEN name_key = 'ekd.roles.client.newsEditor.name'
-                                    THEN 'Новостник' END)) AS role
+                                    THEN 'Новостник' END)) AS role  
                      , MAX(cur.modified_date)              AS modified_date
                 FROM ekd_ekd.client_user                cu
                      LEFT JOIN ekd_ekd.client_user_role cur ON cu.id = cur.client_user_id
@@ -179,16 +164,14 @@ async def collect_dim_user(conn, from_modified_date, to_modified_date):
     return await conn.fetch(query)
 
 
-async def collect_dim_user_login(conn, from_modified_date, to_modified_date):
-    await conn.execute("SET LOCAL work_mem = '384MB';")
-
+async def collect_dim_user_login_scd2(conn, from_modified_date, to_modified_date):
     query = f"""
-        SELECT user_login.user_id                 AS user_id
-             , user_login.login_type              AS login_type
-             , user_login.login                   AS login_value
-             , notification_channel.enabled       AS active
-             , user_login.confirmed_date          AS confirmed_date
-             , notification_channel.modified_date AS modified_date
+        SELECT user_login.user_id                               AS user_id
+             , user_login.login_type                            AS login_type
+             , user_login.login                                 AS login_value
+             , notification_channel.enabled                     AS active
+             , user_login.confirmed_date::TIMESTAMP(0)          AS confirmed_date
+             , notification_channel.modified_date::TIMESTAMP(0) AS modified_date
         FROM ekd_id.user_login
              JOIN ekd_id.notification_channel ON user_login.id = notification_channel.user_login_id
         WHERE user_login.confirmed_date IS NOT NULL
@@ -199,8 +182,6 @@ async def collect_dim_user_login(conn, from_modified_date, to_modified_date):
 
 
 async def collect_dim_user_role(conn, from_modified_date, to_modified_date):
-    await conn.execute("SET LOCAL work_mem = '384MB';")
-
     query = f"""
     WITH
         u_roles AS (
@@ -255,5 +236,31 @@ async def collect_dim_user_role(conn, from_modified_date, to_modified_date):
          FULL JOIN e_roles_pivot USING (user_id)
     WHERE ((u_roles_pivot.modified_date >= '{from_modified_date}' AND u_roles_pivot.modified_date < '{to_modified_date}')
         OR (e_roles_pivot.modified_date >= '{from_modified_date}' AND e_roles_pivot.modified_date < '{to_modified_date}'));
+    """
+    return await conn.fetch(query)
+
+
+
+async def collect_agg_e_doc_type_m(conn, from_modified_date, to_modified_date):
+    query = f"""
+        SELECT date_trunc('month', document.created_date)::DATE as start_of_month
+             , COUNT (DISTINCT document_type_id) as agg_cnt_doc_type
+        FROM ekd_ekd.document
+        WHERE base_document_id IS NOT NULL AND docflow_finished_date IS NOT NULL
+          AND sent_date >= '{from_modified_date}' AND sent_date <= '{to_modified_date}'
+        GROUP BY start_of_month;
+    """
+    return await conn.fetch(query)
+
+
+async def collect_agg_e_app_type_m(conn, from_modified_date, to_modified_date):
+    query = f"""
+        SELECT date_trunc('month', application.created_date)::DATE as start_of_month
+             , COUNT(DISTINCT application_type_id) as agg_cnt_app_type
+        FROM ekd_ekd.application
+             JOIN ekd_ekd.application_group ON application.application_group_id = application_group.id
+        WHERE docflow_finished_date IS NOT NULL
+          AND application.created_date >= '{from_modified_date}' AND application.created_date <= '{to_modified_date}'
+        GROUP BY start_of_month;
     """
     return await conn.fetch(query)
