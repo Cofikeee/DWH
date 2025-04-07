b12136afee0fd0c1ed2ab178533f300ade98892d
Petr Bushmanov <bushmanov.pa@gmail.com>
2025-04-07 21:10:55 +0300

    .

--- dags/dags_tenant/dag_collect_ten_session_day.py
+++ dags/dags_tenant/dag_collect_ten_session_day.py
@@ -14,12 +14,19 @@ async def main():
     Основная асинхронная функция для выполнения DAG.
     """
     # Инициализация логгера
-    logger = fl.setup_logger('dag_collect_ten_session_day')
+    import logging
+    logger = logging
+    #logger = fl.setup_logger('dag_collect_ten_session_day')
     logger.info('---------------------------------------------')
     logger.info('Начало работы DAG dag_collect_ten_session_day')
     schema_name = 'dwh_ten'
     table_name = 'agg_s_session_d'
     await ft.crawler(logger=logger, schema_name=schema_name, table_name=table_name, copy=True)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
 
 
 def run_async_func():
